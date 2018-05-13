eb6b1f1f608aa302d2652c3167da1f0c3f32167a
root <vardhamangopani@gmail.com>
2018-05-13 00:09:22 -0700

    Unoptimized solution to count bit difference. Added question in the comment

--- setBitDifferenceUnOptimized.c
+++ setBitDifferenceUnOptimized.c
@@ -1,3 +1,10 @@
+/*
+Sum of bit differences among all pairs
+Given an integer array of n integers, find sum of bit differences in all pairs that can be formed from array 
+elements. Bit difference of a pair (x, y) is count of different bits at same positions in binary representations of x and y. 
+For example, bit difference for 2 and 7 is 2. Binary representation of 2 is 010 and 7 is 111 ( first and last bits differ in two numbers).
+*/
+
 #include<stdio.h>
 int setBitDifference(int arr[], int n){
 	int ans=0;
